3dd58bee22cf6abba0af4b2bc2f1773adebf44ec
Gulyx <guglielmo.deangelis@isti.cnr.it>
2017-01-31 13:03:31 +0100

    Improved Documentation

--- bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/util/RevisedHashSet.java
+++ bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/util/RevisedHashSet.java
@@ -8,6 +8,18 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+/*
+ * Provided a revised implementation of the HashSet, since the one from Java8 was
+ * not behaving in the expected way when dealing with Nodes. Specifically the official implementation
+ * consider that 2 objs instances form Node with the referring the same AnyResource
+ * are different. Specifically the insertion in the internal table of an HashSet exploits
+ * the "hash" method in order to check if the element is present, and which of course my scenario
+ * would return different values (independently form any override of "equals").
+ * This interpretation should be fine in the general case, but for the moment I 
+ * would prefer to consider them "Equals". For further detail consider also the specification
+ * of the element "it.cnr.iasi.leks.bedspread.Node".
+ *
+ */
 public class RevisedHashSet implements Set<Node> {
 
 	private Set<Node> internalSet;
