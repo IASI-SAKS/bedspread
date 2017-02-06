34473f3b7f8dd10b504ba67852bc5a2bbfa91796
Gulyx <guglielmo.deangelis@isti.cnr.it>
2017-02-06 15:57:49 +0100

    Refined the implementation of the Factory. Now the API are more meaningful

--- bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/SematicSpreadFactory.java
+++ bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/SematicSpreadFactory.java
@@ -18,13 +18,9 @@
  */
 package it.cnr.iasi.leks.bedspread;
 
-import it.cnr.iasi.leks.bedspread.impl.SimpleKnowledgeBase;
 import it.cnr.iasi.leks.bedspread.impl.SimpleSemanticSpread;
-import it.cnr.iasi.leks.bedspread.policies.SimpleTerminationPolicy;
 import it.cnr.iasi.leks.bedspread.policies.TerminationPolicy;
-import it.cnr.iasi.leks.bedspread.rdf.AnyResource;
 import it.cnr.iasi.leks.bedspread.rdf.KnowledgeBase;
-import it.cnr.iasi.leks.bedspread.rdf.impl.RDFFactory;
 
 public class SematicSpreadFactory {
 
@@ -40,25 +36,9 @@ public class SematicSpreadFactory {
 		return FACTORY;
 	}
 
-	public AbstractSemanticSpread getSemanticSpread(String uriIdentifier){
-		AnyResource resource = RDFFactory.getInstance().createURI(uriIdentifier);
-	
-		AbstractSemanticSpread s = this.getSemanticSpread(resource);
-		return s;
-	}
-	
-	public AbstractSemanticSpread getSemanticSpread(AnyResource resource){
-		Node origin = new Node(resource); 
-	
-		AbstractSemanticSpread s = this.getSimpleSemanticSpread(origin);
-		return s;
-	}
-
-	private AbstractSemanticSpread getSimpleSemanticSpread(Node origin){
-		KnowledgeBase kb = new SimpleKnowledgeBase();
-		TerminationPolicy term = new SimpleTerminationPolicy();
-	
+	public AbstractSemanticSpread getSemanticSpread(Node origin, KnowledgeBase kb, TerminationPolicy term){
 		AbstractSemanticSpread semSpread = new SimpleSemanticSpread(origin, kb, term);
 		return semSpread;
 	}
+
 }
