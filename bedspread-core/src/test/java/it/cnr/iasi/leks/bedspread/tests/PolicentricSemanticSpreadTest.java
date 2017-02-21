42736936dc00489f53f39fe5f538c78da64d27d8
Gulyx <guglielmo.deangelis@isti.cnr.it>
2017-02-21 11:13:01 +0100

    Improved test set supporting PolicentricSemanticSpread

---
+++ bedspread-core/src/test/java/it/cnr/iasi/leks/bedspread/tests/PolicentricSemanticSpreadTest.java
@@ -0,0 +1,84 @@
+package it.cnr.iasi.leks.bedspread.tests;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import it.cnr.iasi.leks.bedspread.AbstractSemanticSpread;
+import it.cnr.iasi.leks.bedspread.PolicentricSemanticSpread;
+import it.cnr.iasi.leks.bedspread.TerminationPolicy;
+import it.cnr.iasi.leks.bedspread.Node;
+import it.cnr.iasi.leks.bedspread.config.PropertyUtil;
+import it.cnr.iasi.leks.bedspread.exceptions.impl.InteractionProtocolViolationException;
+import it.cnr.iasi.leks.bedspread.impl.policies.SimpleTerminationPolicy;
+import it.cnr.iasi.leks.bedspread.rdf.AnyResource;
+import it.cnr.iasi.leks.bedspread.rdf.KnowledgeBase;
+import it.cnr.iasi.leks.bedspread.rdf.impl.RDFFactory;
+import it.cnr.iasi.leks.bedspread.rdf.impl.RDFGraph;
+import it.cnr.iasi.leks.bedspread.util.SetOfNodesFactory;
+import it.cnr.iasi.leks.debspread.tests.util.PropertyUtilNoSingleton;
+
+public class PolicentricSemanticSpreadTest extends AbstractTest{
+	
+	private RDFGraph rdfGraph;
+	private static final String ORIGIN_PREFIX_LABEL = "origin";
+	private static final int NUMBER_OF_ORIGINS = 2;
+
+	private static final String INPUT_GRAPH_FILE = "src/test/resources/whiteboardTwoOriginsRDFGraph.csv";
+	
+	
+	@Test
+	public void firstMinimalTestConf() throws IOException, ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InterruptedException, InteractionProtocolViolationException {
+		KnowledgeBase kb = this.loadMinimalKB();
+		Set<Node> resourceOriginSet = this.extractTrivialOriginSet();
+		
+		String testPropertyFile = "configTestPolicentricDefaultWeigh.properties";
+		System.getProperties().setProperty(PropertyUtil.CONFIG_FILE_LOCATION_LABEL, testPropertyFile);
+		PropertyUtilNoSingleton.getInstance();
+		
+		PolicentricSemanticSpread pool = new PolicentricSemanticSpread(resourceOriginSet, kb);
+		pool.startAndTrackProcessing();
+		
+		List<AbstractSemanticSpread> list = pool.getCompletedSemanticSpreadList();
+		String semantiSpreadClassName = PropertyUtil.getInstance().getProperty(PropertyUtil.SEMANTIC_SPREAD_LABEL);
+		boolean condition = ( semantiSpreadClassName != null );
+		for (AbstractSemanticSpread ss : list) {
+			String originID = ss.getOrigin().getResource().getResourceID();
+			String fileName = this.getFlushFileName("firstMinimalTestConf_"+originID);
+			Writer out = new FileWriter(fileName);
+			ss.flushData(out);			
+
+			condition = condition && (ss.getClass().getName().equalsIgnoreCase(semantiSpreadClassName));				
+		}
+
+		System.getProperties().remove(PropertyUtil.CONFIG_FILE_LOCATION_LABEL);
+		Assert.assertTrue(condition);				
+	}
+	
+	private Set<Node> extractTrivialOriginSet() {
+		Set<Node> s = SetOfNodesFactory.getInstance().getSetOfNodesInstance();
+		
+		for(int i=0; i < NUMBER_OF_ORIGINS; i++){
+			String originLabel = ORIGIN_PREFIX_LABEL + i;
+			AnyResource resource = RDFFactory.getInstance().createBlankNode(originLabel);
+			Node n = new Node(resource);
+			s.add(n);
+		}
+		return s;
+	}
+
+	private KnowledgeBase loadMinimalKB() throws IOException{
+		FileReader kbReader = new FileReader(INPUT_GRAPH_FILE);
+		this.rdfGraph = new RDFGraph(kbReader);
+		
+		return this.rdfGraph;
+	}
+
+}
