e92da7fb61bae8ceb29bf4c6ba465c18644fb858
Gulyx <guglielmo.deangelis@isti.cnr.it>
2017-02-22 16:14:54 +0100

    Changed Licence Header

--- bedspread-core/src/test/java/it/cnr/iasi/leks/bedspread/tests/PolicentricSemanticSpreadTest.java
+++ bedspread-core/src/test/java/it/cnr/iasi/leks/bedspread/tests/PolicentricSemanticSpreadTest.java
@@ -4,16 +4,16 @@
  *	 https://github.com/IASI-LEKS/bedspread
  *	     
  *	 This is free software: you can redistribute it and/or modify
- *	 it under the terms of the GNU Lesser General Public License as 
+ *	 it under the terms of the GNU General Public License as 
  *	 published by the Free Software Foundation, either version 3 of the 
  *	 License, or (at your option) any later version.
  *	 
  *	 This software is distributed in the hope that it will be useful,
  *	 but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	 GNU Lesser General Public License for more details.
+ *	 GNU General Public License for more details.
  * 
- *	 You should have received a copy of the GNU Lesser General Public License
+ *	 You should have received a copy of the GNU General Public License
  *	 along with this source.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.cnr.iasi.leks.bedspread.tests;
@@ -34,7 +34,6 @@ import it.cnr.iasi.leks.bedspread.PolicentricSemanticSpread;
 import it.cnr.iasi.leks.bedspread.Node;
 import it.cnr.iasi.leks.bedspread.config.PropertyUtil;
 import it.cnr.iasi.leks.bedspread.exceptions.AbstractBedspreadException;
-import it.cnr.iasi.leks.bedspread.exceptions.impl.InteractionProtocolViolationException;
 import it.cnr.iasi.leks.bedspread.impl.HT13PolicentricSemanticSpread;
 import it.cnr.iasi.leks.bedspread.rdf.AnyResource;
 import it.cnr.iasi.leks.bedspread.rdf.KnowledgeBase;
