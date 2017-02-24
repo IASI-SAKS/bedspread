9b9588770cebbc6abf967a180a175999e2dc1d45
ftaglino <francesco.taglino@iasi.cnr.it>
2017-02-24 12:58:06 +0100

    1st version of the DBpediaKB class and tests - getNeighborhood: the method counts also resources linked via "rdf:type" predicate.

--- bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/rdf/impl/DBpediaKB.java
+++ bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/rdf/impl/DBpediaKB.java
@@ -3,11 +3,12 @@ package it.cnr.iasi.leks.bedspread.rdf.impl;
  * 
  */
 
-
+import java.util.HashSet;
 import java.util.Set;
 import java.util.Vector;
 
 import org.apache.jena.query.QuerySolution;
+import org.apache.jena.rdf.model.RDFNode;
 
 import it.cnr.iasi.leks.bedspread.rdf.AnyResource;
 import it.cnr.iasi.leks.bedspread.rdf.KnowledgeBase;
@@ -34,8 +35,21 @@ public class DBpediaKB implements KnowledgeBase {
 	 */
 	@Override
 	public int degree(AnyResource resource) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0; 
+		SPARQLEndpointConnector sec = new SPARQLEndpointConnector(DBPEDIA_ENDPOINT);
+		
+		// Search for incoming predicates
+		String queryString = "SELECT (COUNT(*) AS ?count) FROM <"+DBPEDIA_GRAPH+"> WHERE {"
+				+ "{?s ?p <"+resource.getResourceID()+">} "
+				+ "UNION "
+				+ "{<"+resource.getResourceID()+"> ?p ?o}"
+				+ "}";
+		
+		Vector<QuerySolution> query_results = sec.execQuery(queryString);
+		if(query_results.size()>0)
+			result = query_results.elementAt(0).getLiteral("count").asLiteral().getInt();
+		
+		return result;
 	}
 
 	/* (non-Javadoc)
@@ -52,8 +66,23 @@ public class DBpediaKB implements KnowledgeBase {
 	 */
 	@Override
 	public boolean isMemberof(AnyResource resource) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean result = false;
+		SPARQLEndpointConnector sec = new SPARQLEndpointConnector(DBPEDIA_ENDPOINT);
+		Vector<QuerySolution> query_results;
+		if(resource instanceof URIImpl) {
+			String queryString = "SELECT (COUNT(*) AS ?count) FROM <"+DBPEDIA_GRAPH+"> WHERE {"
+					+ "{?s1 ?p1 <"+resource.getResourceID()+">} "
+					+ "UNION "
+					+ "{<"+resource.getResourceID()+"> ?p2 ?o2}"
+					+ "UNION "
+					+ "{ ?s3 <"+resource.getResourceID()+"> ?o3}"
+					+ "}";
+			query_results = sec.execQuery(queryString);
+			if(query_results.size()>0)
+				if(query_results.elementAt(0).getLiteral("count").asLiteral().getInt()>0)
+					result = true;
+		}
+		return result;
 	}
 
 	/* (non-Javadoc)
@@ -61,30 +90,63 @@ public class DBpediaKB implements KnowledgeBase {
 	 */
 	@Override
 	public Set<AnyResource> getNeighborhood(AnyResource resource) {
-		// TODO Auto-generated method stub
-		return null;
+		Set<AnyResource> result = getIncomingNeighborhood(resource);
+		result.addAll(getOutgoingNeighborhood(resource));
+		return result;
 	}
 	
 	/**
 	 * Return all the resources that play the role of SUBJECT in the triples having the passed resource as the OBJECT
+	 * If more than a triple having the same SUBJECT and OBJECT exists, this is not recorded (the returning type is a Set and not a Vector. 
 	 * @param resource
-	 * @return THINK ABOUT A VECTOR OR A SET
+	 * @return 
 	 */
-	public Vector<AnyResource> getIncomingNeighborhood(AnyResource resource) {
-		Vector<AnyResource> result = new Vector<AnyResource>();
+	public Set<AnyResource> getIncomingNeighborhood(AnyResource resource) {
+		Set<AnyResource> result = new HashSet<AnyResource>();
 		SPARQLEndpointConnector sec = new SPARQLEndpointConnector(DBPEDIA_ENDPOINT);
 		
 		String queryString = "SELECT ?s FROM <"+DBPEDIA_GRAPH+"> WHERE {"
 				+ "?s ?p <"+resource.getResourceID()+">"
 				+ "}";
 		
-		Vector<QuerySolution> qss = sec.execQuery(queryString);
+		Vector<QuerySolution> query_results = sec.execQuery(queryString);
 		
-		for(int i=0; i<qss.size(); i++) {
-			String neighboor = qss.elementAt(i).getResource("s").getURI().toString();  
+		for(int i=0; i<query_results.size(); i++) {
+			String neighboor = query_results.elementAt(i).getResource("s").getURI().toString();  
 			result.add(new URIImpl(neighboor)); 
  		}		
 		return result;
 	}
 
+	/**
+	 * Return all the resources that play the role of OBJECT in the triples having the passed resource as the SUBJECT
+	 * If more than a triple having the same SUBJECT and OBJECT exists, this is not recorded (the returning type is a Set and not a Vector. 
+	 * @param resource
+	 * @return 
+	 */
+	public Set<AnyResource> getOutgoingNeighborhood(AnyResource resource) {
+		Set<AnyResource> result = new HashSet<AnyResource>();
+		SPARQLEndpointConnector sec = new SPARQLEndpointConnector(DBPEDIA_ENDPOINT);
+		
+		String queryString = "SELECT ?o FROM <"+DBPEDIA_GRAPH+"> WHERE {"
+				+ "<"+resource.getResourceID()+"> ?p ?o "
+				+ "}";
+		
+		Vector<QuerySolution> query_results = sec.execQuery(queryString);
+		
+		for(int i=0; i<query_results.size(); i++) {
+			RDFNode node = query_results.elementAt(i).get("o");
+			if(node.isResource()) {
+				String neighboor = node.asResource().getURI().toString();  
+				result.add(new URIImpl(neighboor));
+			}
+			if(node.isLiteral()) {
+				String neighboor = node.asLiteral().toString();
+				result.add(new LiteralImpl(neighboor));
+			}
+ 		}		
+		return result;
+	}
+	
+	
 }
