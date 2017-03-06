465290837c93506be67b171ea272b7681d56790d
ftaglino <francesco.taglino@iasi.cnr.it>
2017-03-06 16:07:37 +0100

    License and author added

--- bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/impl/weights/SemanticWeighting_IC.java
+++ bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/impl/weights/SemanticWeighting_IC.java
@@ -1,3 +1,21 @@
+/*
+ * 	 This file is part of Bedspread, originally promoted and
+ *	 developed at CNR-IASI. For more information visit:
+ *	 https://github.com/IASI-LEKS/bedspread
+ *	     
+ *	 This is free software: you can redistribute it and/or modify
+ *	 it under the terms of the GNU General Public License as 
+ *	 published by the Free Software Foundation, either version 3 of the 
+ *	 License, or (at your option) any later version.
+ *	 
+ *	 This software is distributed in the hope that it will be useful,
+ *	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	 GNU General Public License for more details.
+ * 
+ *	 You should have received a copy of the GNU General Public License
+ *	 along with this source.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.cnr.iasi.leks.bedspread.impl.weights;
 
 import java.util.Set;
@@ -5,14 +23,16 @@ import java.util.Set;
 import it.cnr.iasi.leks.bedspread.Node;
 import it.cnr.iasi.leks.bedspread.WeightingFunction;
 import it.cnr.iasi.leks.bedspread.rdf.AnyResource;
-import it.cnr.iasi.leks.bedspread.rdf.AnyURI;
-import it.cnr.iasi.leks.bedspread.rdf.KnowledgeBase;
-import it.cnr.iasi.leks.bedspread.rdf.URI;
 import it.cnr.iasi.leks.bedspread.rdf.impl.DBpediaKB;
 import it.cnr.iasi.leks.bedspread.rdf.impl.EdgeWeighting_IC;
 import it.cnr.iasi.leks.bedspread.rdf.impl.RDFTriple;
 import it.cnr.iasi.leks.bedspread.rdf.impl.URIImpl;
 
+/**
+ * 
+ * @author ftaglino
+ *
+ */
 public class SemanticWeighting_IC implements WeightingFunction {
 
 	DBpediaKB kb;
