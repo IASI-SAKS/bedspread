899c4ae621f88116b318061a64ad2268fa42c023
Gulyx <guglielmo.deangelis@isti.cnr.it>
2017-04-28 11:26:17 +0200

    Updated URL in the licence header

--- bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/rdf/impl/BlankNodeImpl.java
+++ bedspread-core/src/main/java/it/cnr/iasi/leks/bedspread/rdf/impl/BlankNodeImpl.java
@@ -1,7 +1,7 @@
 /*
  * 	 This file is part of Bedspread, originally promoted and
  *	 developed at CNR-IASI. For more information visit:
- *	 https://github.com/IASI-LEKS/bedspread
+ *	 http://leks.iasi.cnr.it/tools/bedspread
  *	     
  *	 This is free software: you can redistribute it and/or modify
  *	 it under the terms of the GNU General Public License as 
