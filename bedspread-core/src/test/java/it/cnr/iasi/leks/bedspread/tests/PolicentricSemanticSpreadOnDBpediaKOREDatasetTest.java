6e63a712f609f58ca265419ee1f4f57d22e65ff5
Gulyx <guglielmo.deangelis@isti.cnr.it>
2017-06-09 18:06:07 +0200

    Improved Test KORE

--- bedspread-core/src/test/java/it/cnr/iasi/leks/bedspread/tests/PolicentricSemanticSpreadOnDBpediaKOREDatasetTest.java
+++ bedspread-core/src/test/java/it/cnr/iasi/leks/bedspread/tests/PolicentricSemanticSpreadOnDBpediaKOREDatasetTest.java
@@ -59,12 +59,13 @@ public class PolicentricSemanticSpreadOnDBpediaKOREDatasetTest extends AbstractT
 	private static final int KORE_DATASET_MAX_ORIGINS = 4;
 	private static final int KORE_DATASET_FIRST_ORIGIN_INDEX = 4;
 
-//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/0_KORE_modified _Apple.txt";
-	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/1_KORE_modified _ITCompanies.txt";
-//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/2_KORE_modified _Hollywood.txt";
-//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/3_KORE_modified _VideoGames.txt";
-//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/4_KORE_modified _TelevisionSeries.txt";
-//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/5_KORE_modified _ChuckNorris.txt";
+//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/0_KORE_modified_Apple.txt";
+	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/0_KORE_modified_AppleGoogle.txt";
+//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/1_KORE_modified_ITCompanies.txt";
+//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/2_KORE_modified_Hollywood.txt";
+//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/3_KORE_modified_VideoGames.txt";
+//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/4_KORE_modified_TelevisionSeries.txt";
+//	private static final String INPUT_KORE_DATASET_FILE = "src/test/resources/datasets-KORE/5_KORE_modified_ChuckNorris.txt";
 
 	private static HashMap<String, List<String>> KORE_DATASET_MAP;
 	
